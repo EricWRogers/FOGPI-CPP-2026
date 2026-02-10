9deb7cbfc215ca7fa256c549b430212f9af18a23
EricWRogers <ericwilliamrogers@gmail.com>
2026-02-10 12:13:57 -0600

    Vector Example

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,14 @@
 
 #include <iostream>
 
+#include "fogpi/Math.hpp"
+
 int main(int argc, char *argv[])
 {
     std::cout << "Hello, Class" << std::endl;
+
+    Vec2 v;
+    v.x = 5.0f;
 
     // random seed
     
